8cd06ddae82fdedaf06cce2fc74a32aad5e30767
Sejal Pagare <sejalpagare@gmail.com>
2023-08-09 09:29:59 +0000

    Initial stage

---
+++ src/main/java/com/springdemo/db/PBWMChallenge/PbwmChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.springdemo.db.PBWMChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbwmChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbwmChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springdemo/db/PBWMChallenge/PbwmChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.db.PBWMChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbwmChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
